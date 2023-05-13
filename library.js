16fe584fc88851f221a99454d14aae637d4295b2
idX401 <46228103+idX401@users.noreply.github.com>
2023-05-13 20:59:20 +0300

    UserData fix

--- library.js
+++ library.js
@@ -14,7 +14,7 @@ plugin.alterContent = async function (params, callback) {
 			);
 		}
 	}else{
-		let userData = getUser(params.caller.uid);
+		let userData = await getUser(params.caller.uid);
 		console.log(params,userData);
 	}
   callback(null, params);
