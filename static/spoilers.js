5baed627811222f69f09fe217aacea4c212edcb3
idX401 <46228103+idX401@users.noreply.github.com>
2023-07-17 11:01:12 +0300

    Create spoilers.js

---
+++ static/spoilers.js
@@ -0,0 +1,27 @@
+$('document').ready(function() {
+    require(['composer', 'composer/controls'], function(composer, controls) {
+        composer.addButton('fa fa-flag', function(textarea, selectionStart, selectionEnd) {
+
+            let example = '[SPOILER="Title"]\nTest\n[/SPOILER]'
+            if(selectionStart === selectionEnd){
+                controls.insertIntoTextarea(textarea, example);
+                controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionStart + 11 + spoilersTitle.length);
+            } else {
+                controls.wrapSelectionInTextareaWith(textarea, example);
+                controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionEnd + 11 + spoilersTitle.length);
+            }
+        });
+    });
+
+    $('body').on('click', 'div.show-spoiler', function(){
+        $(this).find('.fa').toggleClass('fa-eye');
+        $(this).find('.fa').toggleClass('fa-eye-slash');
+        if($(this).find('.fa').hasClass('fa-eye')){
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
+        } else {
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
+        }
+        $(this).parent().find('> .spoiler').toggleClass('hidden');
+    });
+
+});
