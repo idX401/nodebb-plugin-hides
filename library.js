35ad353316dd1566e25ffb940823fd6d1d42f719
idX401 <46228103+idX401@users.noreply.github.com>
2023-07-17 11:00:01 +0300

    Update library.js

--- library.js
+++ library.js
@@ -30,4 +30,30 @@ plugin.getUser = async function (uid) {
 	return await user.getUserFields(uid, ['username', 'userslug', 'status', 'postcount', 'reputation', 'joindate', 'groupTitle']);
 };
 
+plugin.parseContent = async  function(data, callback) {
+    var Transform = async function (content) {
+      return content.replace(/\[SPOILER\]/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="Сlick to show or hide"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="spoiler" data-hide_text="spoiler">spoiler</span></div><div class="spoiler hidden">')
+	    .replace(/\[SPOILER=([^<]*)\]/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="Сlick to show or hide"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="$1" data-hide_text="$1">$1</span></div><div class="spoiler hidden">')
+	    .replace(/\[\/SPOILER\]/g, '</div></div>');
+    };
+
+    if ('string' === typeof data) {
+      Transform(data).then((parsedContent) => {
+	data = parsedContent;
+	callback(null, data);
+      });
+    } else if (data.postData && data.postData.content != null && data.postData.content != undefined) {
+      Transform(data.postData.content).then((parsedContent) => {
+	data.postData.content = parsedContent;
+	callback(null, data);
+      });
+    } else if (data.userData && data.userData.signature != null && data.userData.signature != undefined) {
+      Transform(data.userData.signature).then((parsedContent) => {
+	data.userData.signature = parsedContent;
+	callback(null, data);
+      });
+    }
+}
+
+
 module.exports = plugin;
