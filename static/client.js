01d5dab4755d2127e1fce292e5f459c1578bd257
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-10 18:42:13 +0300

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,4 +1,3 @@
-var BBcode = {};
 $('document').ready(function() {
     require(['composer', 'composer/controls'], function(composer, controls) {
         composer.addButton('fa fa-flag', function(textarea, selectionStart, selectionEnd) {
@@ -41,61 +40,16 @@ $('document').ready(function() {
         			{ name: 'italic', className: 'fa fa-italic', text: 'Italic', onClick: fn },
         		],
         	});
-        
-        $('body').on('click', 'div.show-spoiler', function(){
-            $(this).find('.fa').toggleClass('fa-eye');
-            $(this).find('.fa').toggleClass('fa-eye-slash');
-            if($(this).find('.fa').hasClass('fa-eye')){
-                $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
-            } else {
-                $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
-            }
-            $(this).parent().find('> .spoiler').toggleClass('hidden');
-        });
+
+    $('body').on('click', 'div.show-spoiler', function(){
+        $(this).find('.fa').toggleClass('fa-eye');
+        $(this).find('.fa').toggleClass('fa-eye-slash');
+        if($(this).find('.fa').hasClass('fa-eye')){
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
+        } else {
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
+        }
+        $(this).parent().find('> .spoiler').toggleClass('hidden');
     });
 
-	BBcode.render = function (ev, data) {
-        
-		if (!data.posts && !data.post) {
-			return;
-		} if (data.hasOwnProperty('post')) {
-			data.posts = [data.post];
-		}
-
-		var disable;
-		var checkboxEls;
-		data.posts.forEach(function (post) {
-			disable = !post.display_edit_tools;
-			checkboxEls = $('.posts li[data-pid="' + post.pid + '"] .content div.plugin-markdown input[type="checkbox"]');
-
-			checkboxEls.on('click', function (e) {
-				if (disable) {
-					// Find the post's checkboxes in DOM and make them readonly
-					e.preventDefault();
-				}
-
-				// Otherwise, edit the post to reflect state change
-				var _this = this;
-				var pid = $(this).parents('li[data-pid]').attr('data-pid');
-				var index = $(this).parents('.content').find('input[type="checkbox"]').toArray()
-					.reduce(function (memo, cur, index) {
-						if (cur === _this) {
-							memo = index;
-						}
-
-						return memo;
-					}, null);
-
-				socket.emit('plugins.markdown.checkbox.edit', {
-					pid: pid,
-					index: index,
-					state: $(_this).prop('checked'),
-				});
-			});
-		});
-	};
-/*
-$(window).on('action:topic.loaded', BBcode.render);
-$(window).on('action:posts.loaded', BBcode.render);
-$(window).on('action:posts.edited', BBcode.render);
-*/
+});
