b2eb7a4cc85bd405ed4cb2bc7caa24c327db70be
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-10 19:24:38 +0300

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,7 +1,6 @@
 $('document').ready(function() {
     require(['composer', 'composer/controls'], function(composer, controls) {
         composer.addButton('fa fa-flag', function(textarea, selectionStart, selectionEnd) {
-
             let example = '[SPOILER="Title"]\nTest\n[/SPOILER]'
             if(selectionStart === selectionEnd){
                 controls.insertIntoTextarea(textarea, example);
@@ -11,6 +10,7 @@ $('document').ready(function() {
                 controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionEnd + 11 + spoilersTitle.length);
             }
         });
+        
         composer.addButton('fa fa-bold', function(textarea, selectionStart, selectionEnd) {
             let example = '[B]\nBold text\n[/B]'
             if(selectionStart === selectionEnd){
@@ -21,6 +21,7 @@ $('document').ready(function() {
                 controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionEnd + 11 + spoilersTitle.length);
             }
         }, 'strong text', 'Bold');
+        
         function fn(textarea, selectionStart, selectionEnd) {
             let example = '[B]\nBold text\n[/B]'
             if(selectionStart === selectionEnd){
@@ -40,16 +41,16 @@ $('document').ready(function() {
         			{ name: 'italic', className: 'fa fa-italic', text: 'Italic', onClick: fn },
         		],
         	});
-    
-        $('body').on('click', 'div.show-spoiler', function(){
-            $(this).find('.fa').toggleClass('fa-eye');
-            $(this).find('.fa').toggleClass('fa-eye-slash');
-            if($(this).find('.fa').hasClass('fa-eye')){
-                $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
-            } else {
-                $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
-            }
-            $(this).parent().find('> .spoiler').toggleClass('hidden');
-        });
+        
     });
+    $('body').on('click', 'div.show-spoiler', function() {
+        $(this).find('.fa').toggleClass('fa-eye');
+        $(this).find('.fa').toggleClass('fa-eye-slash');
+        if($(this).find('.fa').hasClass('fa-eye')){
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
+        } else {
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
+        }
+        $(this).parent().find('> .spoiler').toggleClass('hidden');
+    });  
 });
