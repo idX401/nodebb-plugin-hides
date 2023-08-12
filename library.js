3f486758398071e0ea9ce73be083d96a13f4ae1b
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-12 19:31:40 +0300

    Update library.js

--- library.js
+++ library.js
@@ -189,7 +189,7 @@ plugin.parseContent = async function(data) {
  	*/
 	function parseP(text){
 	    //return text.replace(/\n(.+)\n/gi,'<p dir="auto">$1</p>\n');
-		return text.replace(/\n/g,'<br>');
+	    return text.replace(/\n(.+)\n/gi,'<br>$1\n');
 	}
 	function parseBolt(text) {
 	    while(text.search(boltRegex) !== -1) {
