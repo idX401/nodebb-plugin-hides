ba9ef6a8c53a6f0f344e78f148e93ccf4ad7748b
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-14 19:10:24 +0300

    Update library.js

--- library.js
+++ library.js
@@ -432,7 +432,7 @@ plugin.parseContent = async function(data) {
 		} else if (data.userData && data.userData.signature != null && data.userData.signature != undefined) {
 			data.userData.signature = renderPosts(data.userData.signature);
 		}
-	}else if (data.caller.originalUrl.match(/user\/(.*)\/posts/) != null) {
+	}else if ( data.caller && data.caller.originalUrl && data.caller.originalUrl.match(/user\/(.*)\/posts/) != null) {
 		if (data.postData && data.postData.content != null && data.postData.content != undefined) {
 			data.postData.content = renderPosts(data.postData.content);
 		} else if (data.userData && data.userData.signature != null && data.userData.signature != undefined) {
